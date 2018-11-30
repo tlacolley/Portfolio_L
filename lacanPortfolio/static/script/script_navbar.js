b78bf80e2ab3ee6c019138c488c261836cf29d49
tlacolley <thomaslacolley@gmail.com>
2018-11-30 16:56:40 +0100

    Creation base file for navbar animation

---
+++ lacanPortfolio/static/script/script_navbar.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+
+
+
+var $navbar = $("#navbar"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+
+    $(document).scroll(function() {
+        var scrollTop = $(this).scrollTop();
+
+        if (scrollTop > y_pos + height) {
+            $navbar.addClass("navbar-fixed").animate({
+              top:0
+            });
+
+        } else if (scrollTop <= y_pos) {
+            $navbar.removeClass("navbar-fixed").clearQueue().animate({
+                top: "-48px"
+            }, 0);
+        }
+    });
+
+
+
+
+
+
+})
