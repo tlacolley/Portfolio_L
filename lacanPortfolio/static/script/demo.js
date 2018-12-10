91960c2bf987b0668a8c394216517b1d6e167356
tlacolley <thomaslacolley@gmail.com>
2018-12-10 12:06:15 +0100

    Test First scene 3D works. Issues with import texture, Script need be in base.html

--- lacanPortfolio/static/script/demo.js
+++ lacanPortfolio/static/script/demo.js
@@ -1,3 +1,5 @@
+           
+
 
 "use strict";
 
@@ -26,16 +28,68 @@ function runDemo(canvasId){
 
   // Ajout d'une lumière
   var light = new BABYLON.PointLight("DirLight", new BABYLON.Vector3(0,10,0),scene);
-  light.diffuse = new BABYLON.Color3(1, 1, 1);
+  light.diffuse = new BABYLON.Color3(1, 0.2, 0.2);
   light.specular = new BABYLON.Color3(0.6, 0.6, 0.6);
-  light.intensity = 1.5;
+  light.intensity = 0.5;
 
+
+  function createDemoScene(scene){
+    // Creation d un sol 
+    var ground = BABYLON.Mesh.CreatePlane("ground", 50, scene);
+    ground.rotation.x = Math.PI / 2;
+    ground.material = new BABYLON.StandardMaterial("gMaterial",scene);
+    ground.material.diffuseTexture = new BABYLON.Texture("{% static 'img/ground.jpg' %}",scene);
+    ground.checkCollisions = true; 
+    // Et quelques cubes...
+    var boxMaterial = new BABYLON.StandardMaterial("boxMaterial",scene);
+    boxMaterial.diffuseTexture = new BABYLON.Texture("{% static 'img/ground.jpg' %}",scene);
+
+    var position = [
+                { x:-15, z:15},
+                { x:-15, z:-15},
+                { x:15, z:15},
+                { x:15, z:-15}
+    ];
+
+    var cubeSize = 2.5;
+
+    for( var i =0; i<4; i++){
+      var box = BABYLON.Mesh.CreateBox("box1", cubeSize, scene);
+      box.position = new BABYLON.Vector3(position[i].x, cubeSize / 2, position[i].z);
+      box.material = boxMaterial;
+      box.checkCollisions = true;
+    }
+
+
+  }
+  function createSkybox(scene){
+    // Creation d une material 
+    var sMaterial = new BABYLON.StandardMaterial("skyboxMaterial", scene);
+    sMaterial.backFaceCulling = false;
+    sMaterial.reflectionTexture = new BABYLON.CubeTexture("{% static 'img/Skybox/sky' %}",scene);
+    sMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    // Création d'un cube avec la material adaptée
+    var skybox = BABYLON.Mesh.CreateBox("skybox", 250, scene);
+    skybox.material = sMaterial;
+
+  }
+//   function createSkybox(scene) {
+//   // Création d'une material
+//      var sMaterial = new BABYLON.StandardMaterial("skyboxMaterial", scene);
+//      sMaterial.backFaceCulling = false;
+//      sMaterial.reflectionTexture = new BABYLON.CubeTexture("{% static 'img/sky.jpg' %}",scene);
+//      sMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+
+//      // Création d'un cube avec la material adaptée
+//      var skybox = BABYLON.Mesh.CreateBox("skybox", 250, scene);
+//      skybox.material = sMaterial;
+// }
+    createDemoScene();
+   createSkybox(scene);
   // Lancement de la boucle principale
   engine.runRenderLoop(function() {
       scene.render();
    });
-
-  
 
 }
 
